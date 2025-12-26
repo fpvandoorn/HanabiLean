1be9741e777ff25812c0b3831e75a68fe362918e
Floris van Doorn <fpvdoorn@gmail.com>
2025-12-26 14:34:39 +0100

    continue. define chop-focused play clues as a convention

--- Hanabi/Game.lean
+++ Hanabi/Game.lean
@@ -1,4 +1,6 @@
-import Mathlib.Data.Real.Basic
+import Mathlib.Data.Real.Archimedean
+
+set_option linter.unusedVariables false
 
 /-! # General games -/
 
@@ -9,14 +11,16 @@ structure Game : Type _ where
   Should typically encode the full game history and any future randomness. -/
   State : Type
   /-- The information state a player can have. -/
-  Information : Type
+  Info : Type
   /-- Possible actions. -/
   Action : Type
   /-- The possible set of starting states
   * currently assuming a uniform distribution on these, we could change that. -/
-  startingStates : Set State
+  start : Set State
   /-- The information of a player at a turn. -/
-  info : State → Player → Information
+  info : State → Player → Info
+  /-- Every player knows who they are -/
+  me : Info → Player
   /-- the player whose move it is.
   * We allow for the fact that not everyone knows whose turn it is.
   -/
@@ -27,21 +31,28 @@ structure Game : Type _ where
   * Currently we allow for situations where players might not know
     whether an action of another player is valid.
   -/
-  valid : Information → Set Action
+  valid : Info → Set Action
   /-- the state after an action. -/
   next : State → Action → State
   /-- the score of a player at a state.
   * need not be meaningful when the game is ongoing.
   * doesn't depend on the player-variable in fully cooperative games. -/
   score : State → Player → ℝ
+  [inhabited_state : Inhabited State]
+  [inhabited_action : Inhabited Action]
 
 variable {G : Game}
 
 namespace Game
 
+attribute [instance] inhabited_state inhabited_action
+instance : Inhabited G.Player := ⟨G.current default⟩
+instance : Inhabited G.Info := ⟨G.info default default⟩
+
 def State.info := Game.info G
 def State.current := Game.current G
-def Information.valid := Game.valid G
+def Info.valid := Game.valid G
+def Info.me := Game.me G
 def State.next := Game.next G
 def State.score := Game.score G
 
@@ -65,67 +76,159 @@ def State.IsDecidableBy (S : G.State) (P : G.State → Prop) (p : G.Player) : Pr
 /-!  Some nice properties to have in a game. -/
 
 /-- Everyone knows whether the game has ended. -/
-def KnowGameEnd : Prop := ∀ (S : G.State) (p : G.Player), S.IsDecidableBy State.IsGameEnd p
+def KnowGameEnd (G : Game) : Prop :=
+  ∀ (S : G.State) (p : G.Player), S.IsDecidableBy State.IsGameEnd p
 /-- Every player knows whether it's their turn. -/
-def KnowCurrent : Prop := ∀ (S : G.State) (p : G.Player), S.IsDecidableBy (State.current · = p) p
-/-- Every player remembers the previous game state and knows the last action. -/
-def GoodKnowledge : Prop :=
-  ∀ ⦃S S' : G.State⦄ ⦃A A' : G.Action⦄ (p : G.Player) (_h : A ∈ S.valid) (_h' : A' ∈ S'.valid)
-  (_eq : (S.next A).info p = (S'.next A').info p), S.info p = S'.info p ∧ A = A'
+def KnowCurrent (G : Game) : Prop :=
+  ∀ (S : G.State) (p : G.Player), S.IsDecidableBy (State.current · = p) p
 
-/-- Exercise: given a game, define a new game where the last action taken is public information. -/
+/-- Every player remembers the previous game state and knows the last action. -/
+class GoodKnowledge (G : Game) where
+  /-- The information a player had last turn -/
+  prev : G.Info → Option G.Info
+  /-- Every player can deduce what player played the last action and which action they played. -/
+  prevAction : G.Info → Option (G.Player × G.Action)
+
+def Info.prev := @GoodKnowledge.prev
+def Info.prevAction := @GoodKnowledge.prevAction
+
+def Info.prev! [GoodKnowledge G] (I : G.Info) : G.Info := I.prev.get!
+def Info.prevAction! [GoodKnowledge G] (I : G.Info) : G.Player × G.Action := I.prevAction.get!
+
+noncomputable def maxScore (p : G.Player) : WithTop ℝ :=
+  ⨆ s : G.State, s.score p
+
+/-- Is state `t` a possible state after performing 1 move in state `s`? -/
+noncomputable def State.IsNext (T S : G.State) : Prop :=
+  T ∈ S.next '' S.valid
+
+/-- Is state `t` reachable from state `s`? -/
+noncomputable def State.Reachable (T S : G.State) : Prop :=
+  Relation.ReflTransGen State.IsNext T S
+
+/-- Maximal possible score for player `p`. -/
+noncomputable def Player.maxScore (p : G.Player) : WithTop ℝ :=
+  ⨆ (S : G.State) (_ : S.IsGameEnd), S.score p
+
+/-- Maximal possible score for player `p` reachable from state `S`. -/
+noncomputable def State.maxScore (S : G.State) (p : G.Player) : WithTop ℝ :=
+  ⨆ (T : G.State) (_ : T.IsGameEnd) (_ : T.Reachable S) , T.score p
+
+
+/-- Given a game, we define a new game where the last action taken (and who took it)
+is public information. -/
 def withAction (G : Game) : Game where
   Player := G.Player
-  State := G.State × Option G.Action
-  Information := G.Information × Option G.Action
+  State := G.State × Option (G.Player × G.Action)
+  Info := G.Info × Option (G.Player × G.Action)
   Action := G.Action
-  startingStates := sorry
-  info := sorry
-  current := sorry
-  valid := sorry
-  next := sorry
-  score := sorry
+  start := (·, none) '' G.start
+  info S p := (G.info S.1 p, S.2)
+  me S := G.me S.1
+  current S := G.current S.1
+  valid S := G.valid S.1
+  next S A := (G.next S.1 A, S.1.current, A)
+  score S := G.score S.1
 
-/-- Exercise: given a game,
-define a new game where every player remembers their previous information. -/
+/-- Given a game, we define a new game where every player remembers their previous information.
+We use `α × List α` to talk about nonempty lists on `α`. -/
 def withPrev (G : Game) : Game where
   Player := G.Player
-  State := List G.State
-  Information := List G.Information
+  State := G.State × List G.State
+  Info := G.Info × List G.Info
   Action := G.Action
-  startingStates := sorry
-  info := sorry
-  current := sorry
-  valid := sorry
-  next := sorry
-  score := sorry
+  start := (·, []) '' G.start
+  info S p := (G.info S.1 p, S.2.map (G.info · p))
+  me S := G.me S.1
+  current S := G.current S.1
+  valid S := G.valid S.1
+  next S A := (G.next S.1 A, S.1 :: S.2)
+  score S := G.score S.1
 
-/- Exercise: given a game `G`, show that `G.withAction.withPrev` satisfies `GoodKnowledge`. -/
+/- Given a game `G`, show that `G.withAction.withPrev` satisfies `GoodKnowledge`. -/
+instance (G : Game) : GoodKnowledge G.withAction.withPrev where
+  prev I := match I.2 with
+    | []    => none
+    | i::is => some (i, is)
+  prevAction I := I.1.2
 
+def Info.orig (S : G.withAction.withPrev.Info) : G.Info := S.1.1
+def State.orig (S : G.withAction.withPrev.State) : G.State := S.1.1
+
+/-!
+# Conventions
+
+A *hanabi*-convention is an agreement between players.
+
+We want to formalize the notion of conventions with three criteria:
+* We should be able to write a convention system with modular conventions
+* We should be able to define some simple conventions without too much effort,
+  and potentially prove things about them.
+
+To make conventions convenient, the players need some additional state
+(e.g. playable cards, chop moved cards).
+Note that this information must be shared between conventions:
+a new convention might add a way to chop move cards,
+and any already existing convention should use the same notion of chop moved cards.
+
+We therefore make the additional state type an argument to conventions:
+then we can easily talk about conventions with the same state.
+-/
 
 /-- A game convention -/
-structure Convention (G : Game) : Type _ where
-  /- Is `A` a conventional action with information `I`?
+structure Convention (G : Game) (Info : Type) : Type _ where
+  /-- Is action `A` a conventional action with information `I` and `I'`?
   True: conventional
   False: unconventional
   none: this convention doesn't apply for this action. -/
-  IsConventional (I : G.Information) (A : G.Action) : Option Prop
+  IsConventional (I : G.Info) (I' : Info) (A : G.Action) : Option Bool
+  /-- Hoe does `me` update their info *after* `p` performs `A`?
+  Here `I` is the game state known by a player *after* the action has occurred. -/
+  update (I : G.Info) (I' : Info) : Info
 
 namespace Convention
+variable {Info : Type} {C : G.Convention Info}
 
 /-- Does a convention apply to the current action? -/
-def Applies (C : G.Convention) (I : G.Information) (A : G.Action) : Prop :=
-  C.IsConventional I A |>.isSome
+def Applies (I : G.Info) (I' : Info) (A : G.Action) : Bool :=
+  C.IsConventional I I' A |>.isSome
 
 /-- Combine multiple conventions, where the priority of a convention is given by the order on `ι`.
-An action is conventional if the largest convention that applies says it is. -/
-def combine {ι : Type} [CompleteLinearOrder ι] (C : ι → Convention G) : Convention G where
-  IsConventional I A :=
+An action is conventional if the largest convention that applies says it is.
+`u` is an update function that is always applied first. -/
+def combine [G.GoodKnowledge] {ι : Type} [CompleteLinearOrder ι] (C : ι → G.Convention Info)
+  (u : Π (I : G.Info) (I' : Info), Info) :
+    G.Convention Info where
+  IsConventional I I' A :=
     /- The relevant convention. Note that when the set over which we take the supremum is empty,
     `combine` still gives the right value `none`. -/
-    let i := sSup {i : ι | C i |>.Applies I A }
-    C i |>.IsConventional I A
+    let i := sSup {i : ι | C i |>.Applies I I' A }
+    C i |>.IsConventional I I' A
+  update I I' :=
+    /- todo: this is not right: the receiving player of the clue cannot always decide *which*
+    convention was used to make this clue conventional.
+    One way to fix this is to have a high-priority convention that encodes the ambiguity.
+    e.g.
+    * color-play clues can be given on a playable card
+    * color-save clues can be given on a critical card (or 2) on chop
+    * The save-convention could have higher-priority, and has to encode that the card is either
+      playable or critical or a 2.
+    -/
+    let i := sSup {i : ι |
+      ∃ pI pA, I.prev = some pI ∧ I.prevAction = some pA ∧
+      (C i).IsConventional pI I' pA.2 == some true }
+    C i |>.update I (u I I')
 
+/--
+Extend the conventional information.
+To do this we need a projection `π` from the new datatype to the old one
+and a modification function `m` such that `m I' I` is the information `I'`,
+but with the part coming from `Info` coming updated with `I`.
+-/
+def extend {Info' : Type} (C : G.Convention Info) (m : Info' → Info → Info')
+  (π : Info' → Info) : G.Convention Info' where
+  IsConventional I I' A := C.IsConventional I (π I') A
+  update I I' := m I' (C.update I (π I'))
 
 end Convention
 
--- Hanabi/Hanabi.lean
+++ Hanabi/Hanabi.lean
@@ -1,13 +1,496 @@
 import Hanabi.Game
+import Mathlib.Logic.Equiv.Finset
+import Mathlib.Algebra.BigOperators.Group.Finset.Basic
+
+
+open Encodable (sortedUniv)
+open List
 
 namespace Game
 
-/-- Exercise: define 2-player no variant hanabi -/
-def twoPlayerNoVariantHanabi : Game := sorry
+/-- A quite general hanabi configuration.
+This encodes most variants on hanab.live.
+It encodes black powder by renumbering the black cards
+(and by modifying `touches` so that rank clues still touch the correct cards). -/
+structure HanabiConfig where
+  /-- Number of players -/
+  players : ℕ
+  [neZero_players : NeZero players]
+  /-- Suits of cards -/
+  Suit : Type
+  [fintypeSuit : Fintype Suit]
+  [encodableSuit : Encodable Suit]
+  [decidableEqSuit : DecidableEq Suit]
+  [inhabitedSuit : Inhabited Suit]
+  /-- Number of ranks. We count ranks from 0 to `ranks - 1` -/
+  ranks : ℕ := 5
+  [neZero_ranks : NeZero ranks]
+  /-- Number of copies of a particular card -/
+  numCards : Suit × Fin ranks → ℕ
+  /-- Possible clues -/
+  Clue : Type
+  /-- Does a clue touch a card? -/
+  touches : Clue → Suit × Fin ranks → Bool
+  handSize : ℕ
+  [neZero_handSize : NeZero handSize]
+  /-- number of strikes until the game strikes out -/
+  strikes : ℕ := 3
+  maxClues : ℕ := 8
+
+namespace HanabiConfig
+
+attribute [instance] fintypeSuit encodableSuit decidableEqSuit inhabitedSuit
+  neZero_ranks neZero_players neZero_handSize
+
+variable (C : HanabiConfig)
+
+abbrev Rank : Type := Fin C.ranks
+def maxRank : C.Rank := ⟨C.ranks - 1, by have := C.neZero_ranks.out; grind⟩
+
+abbrev Card : Type := C.Suit × C.Rank
+
+abbrev Player : Type := Fin C.players
+
+def standardDeck : List C.Card :=
+  sortedUniv C.Suit |>.flatMap
+    fun c ↦ finRange C.ranks |>.flatMap
+      fun r ↦ replicate (C.numCards (c, r)) (c, r)
+
+/-- The public information -/
+structure PublicState where
+  /-- play piles. -/
+  progress : C.Suit → ℕ
+  /-- discard (head is most recently discarded) -/
+  discard : List C.Card
+  strikes : ℕ
+  /-- Number of turns left once the deck is empty. -/
+  extraTurns : Option ℕ
+  /-- Current player -/
+  current : C.Player
+  clues : ℕ
+  /-- Each card has a list of received clues (true: positive info, false: negative info)
+  newest first -/
+  cardInfo : C.Player → List (List (C.Clue × Bool))
+  /-- Card played, discarded or misplayed last turn -/
+  lastCard : Option C.Card
+  /-- The last turn was a misplay -/
+  wasMisplay : Bool
+  deriving Inhabited
+
+/-- The data in a game state -/
+structure StateData extends C.PublicState where
+  /-- remaining deck (head is top). -/
+  deck : List C.Card
+  /-- hand cards (head is newest). -/
+  hand : C.Player → List C.Card
+  deriving Inhabited
+
+/-- A correct hanabi game state (WIP) -/
+structure State extends C.StateData where
+  progress_le (c : C.Suit) : progress c ≤ C.ranks
+  deck_eq_nil : extraTurns.isSome ↔ deck = []
+  length_hand_eq (p : C.Player) (h : extraTurns.isNone) : (hand p).length = C.handSize
+  length_hand_or (p : C.Player) : (hand p).length = C.handSize ∨ (hand p).length = C.handSize - 1
+  strikes_le : strikes ≤ C.strikes
+  isPerm : C.standardDeck.isPerm <|
+    deck ++ discard ++ (finRange C.players).flatMap hand ++
+      (sortedUniv C.Suit).flatMap
+        fun c ↦ (finRange (progress c)).map
+        fun r ↦ (c, r.castLE (progress_le c))
+
+inductive Action where
+  | play (n : Fin C.handSize)
+  | discard (n : Fin C.handSize)
+  | clue (p : C.Player) (c : C.Clue)
+
+instance : Inhabited C.Action := ⟨.discard default⟩
+
+/-- The information of one player -/
+structure Info extends C.PublicState where
+  /-- Which player am I -/
+  me : C.Player
+  /-- hand cards of the other players. -/
+  hand (p : C.Player) (h : p ≠ me) : List C.Card
 
 
-/-- Exercise: define a convention for hanabi -/
-def myConvention : twoPlayerNoVariantHanabi.Convention := sorry
+namespace PublicState
+variable {C}
+variable (S : C.PublicState)
 
+def isGameEnd : Bool :=
+  S.strikes = C.strikes ∨ S.extraTurns = some 0 ∨ S.progress = C.ranks
+
+def score : ℕ :=
+  if S.strikes = C.strikes then 0 else ∑ c : C.Suit, S.progress c
+
+/-- Info about card `n` in `p`'s hand -/
+def info (p : C.Player) (n : ℕ) : List (C.Clue × Bool) :=
+  (S.cardInfo p)[n]?.getD []
+
+/-- Positive clues about card `n` in `p`'s hand -/
+def positiveInfo (p : C.Player) (n : ℕ) : List C.Clue :=
+  S.info p n |>.filterMap fun (c, b) ↦ if b then c else none
+
+/-- Is card `n` in `p`'s hand touched? -/
+def touched (p : C.Player) (n : ℕ) : Bool :=
+  (S.positiveInfo p n).length > 0
+
+/-- Negative clues about card `n` in `p`'s hand -/
+def negativeInfo (p : C.Player) (n : ℕ) : List C.Clue :=
+  S.info p n |>.filterMap fun (c, b) ↦ if b then none else c
+
+/-- Is `c` playable? Note: ranks are 0-indexed. -/
+def playable (c : C.Card) : Bool :=
+  S.progress c.1 == c.2
+
+/-- Is `c` trash? Note: ranks are 0-indexed.
+Does not take into account lost cards of lower rank (todo). -/
+def trash (c : C.Card) : Bool :=
+  S.progress c.1 < c.2
+
+/-- Is `c` useful? Note: ranks are 0-indexed. -/
+def useful (c : C.Card) : Bool :=
+  S.progress c.1 ≥ c.2
+
+/-- Is `c` playable or trash? Note: ranks are 0-indexed. -/
+def playableOrTrash (c : C.Card) : Bool :=
+  S.progress c.1 ≤ c.2
+
+/-- Is `c` lost (all copies of this card are discarded). -/
+def lost (c : C.Card) : Bool :=
+  (S.discard.filter (· == c)).length == C.numCards c
+
+/-- Is `c` critical (or lost). -/
+def critical (c : C.Card) : Bool :=
+  C.numCards c == 1 || (S.discard.filter (· == c)).length ≥ C.numCards c - 1
+
+/-- What card (positions) of the current hand of player `p`
+did the last clue to player `p` touch? -/
+def lastTouched (p : C.Player) : List ℕ :=
+  S.cardInfo p |>.findIdxs (·.head?.elim false (·.2))
+
+end PublicState
+
+namespace StateData
+variable {C}
+variable (S : C.StateData)
+
+def toInfo (p : C.Player) : C.Info :=
+  { S with
+    me := p
+    hand q _ := S.hand q }
+
+/-- Card `n` in `p`'s hand -/
+def card' (p : C.Player) (n : ℕ) : C.Card :=
+  (S.hand p)[n]?.getD default
+
+/-- Card `n` in the current player's hand -/
+def card (n : ℕ) : C.Card :=
+  S.card' S.current n
+
+def next : C.Action → C.StateData := fun
+  | .play n => {
+    progress c := if S.playable (S.card n) ∧ (S.card n).1 = c then
+      S.progress c + 1 else S.progress c
+    discard := if S.playable (S.card n) then S.discard else S.card n :: S.discard
+    strikes := if S.playable (S.card n) then S.strikes else S.strikes + 1
+    extraTurns := if S.deck.length = 1 then some C.players else S.extraTurns.map .pred
+    current := S.current + 1
+    clues := if S.playable (S.card n) ∧ (S.card n).2 = C.maxRank then S.clues + 1 else S.clues
+    deck := S.deck.tail
+    hand p := if p = S.current then S.deck[0]?.toList ++ (S.hand p).eraseIdx n else S.hand p
+    cardInfo p := if p = S.current then [] :: (S.cardInfo p).eraseIdx n else S.cardInfo p
+    lastCard := S.card n
+    wasMisplay := !S.playable (S.card n)
+  }
+  | .discard n => {
+    S with
+    discard := S.card n :: S.discard
+    extraTurns := if S.deck.length = 1 then some C.players else S.extraTurns.map .pred
+    current := S.current + 1
+    clues := S.clues + 1
+    deck := S.deck.tail
+    hand p := if p = S.current then S.deck[0]?.toList ++ (S.hand p).eraseIdx n else S.hand p
+    cardInfo p := if p = S.current then [] :: (S.cardInfo p).eraseIdx n else S.cardInfo p
+    lastCard := S.card n
+    wasMisplay := false
+  }
+  | .clue p c => {
+    S with
+    current := S.current + 1
+    clues := S.clues - 1
+    cardInfo p' := if p = p' then
+      (S.cardInfo p').mapIdx fun n l ↦ (c, C.touches c (S.card' p' n)) :: l else S.cardInfo p'
+    extraTurns := S.extraTurns.map .pred
+    lastCard := none
+    wasMisplay := false
+  }
+
+end StateData
+
+namespace Info
+variable {C}
+variable (S : C.Info)
+
+/-- A valid action. -/
+def isValid : C.Action → Bool
+  | .play _    => ¬ S.isGameEnd
+  | .discard _ => ¬ S.isGameEnd ∧ ¬ S.clues ≠ C.maxClues
+  | .clue p cl  => ¬ S.isGameEnd ∧ S.clues > 0 ∧ ∃ h : p ≠ S.me, ∃ cd ∈ S.hand p h, C.touches cl cd
+
+/-- Which slots will cluing `c` to player `p` touch -/
+def willTouch (p : C.Player) (h : p ≠ S.me) (c : C.Clue) : List ℕ :=
+  S.hand p h |>.findIdxs <| C.touches c
+
+/-- Card `n` of player `p` -/
+def card (p : C.Player) (h : p ≠ S.me) (n : ℕ) : C.Card :=
+  (S.hand p h)[n]?.getD default
+
+/-- Is slot `n` of player `p` playable? -/
+def playable' (p : C.Player) (h : p ≠ S.me) (n : ℕ) : Bool :=
+  S.playable (S.card p h n)
+
+end Info
+
+def toSimpleGame : Game where
+  Player := C.Player
+  State := C.StateData
+  Info := C.Info
+  Action := C.Action
+  start := sorry
+  info := (·.toInfo)
+  me := (·.me)
+  current := (·.current)
+  valid S := { A | S.isValid A }
+  next := (·.next)
+  score S := S.score
+
+def toGame : Game := C.toSimpleGame.withAction.withPrev
+deriving GoodKnowledge
+
+end HanabiConfig
+
+/-- The variants 3-suits, 4-suits, no variant and 6-suit. -/
+structure SimpleVariant where
+  players : ℕ
+  numSuits : ℕ
+  [neZero_players : NeZero players]
+  [neZero_numSuits : NeZero numSuits]
+
+namespace SimpleVariant
+
+attribute [instance] neZero_players neZero_numSuits
+
+protected def C (C : SimpleVariant) : HanabiConfig where
+  players := C.players
+  Suit := Fin C.numSuits
+  numCards c := 3 - (c.2 + 2) / 3
+  Clue := Fin C.numSuits ⊕ Fin 5
+  touches
+    | .inl c, (s, _) => c = s
+    | .inr r, (_, r') => r = r'
+  handSize := if C.players ≤ 3 then 5 else 4
+
+def toGame (C : SimpleVariant) : Game := C.C.toGame
+deriving GoodKnowledge
+-- instance : Coe SimpleVariant HanabiConfig := ⟨(·.C)⟩
+
+end SimpleVariant
+
+def twoPlayerNoVariantHanabi : Game :=
+  (⟨2, 5⟩ : SimpleVariant).C.toGame
+
+namespace HanabiConfig
+namespace PublicState
+
+variable {C : SimpleVariant}
+variable (S : C.C.PublicState)
+
+/-- First color rank clue on card `n` in `p`'s hand -/
+def positiveSuit (p : C.C.Player) (n : ℕ) : Option C.C.Suit :=
+  S.positiveInfo p n |>.findSome? fun c ↦ c.getLeft?
+
+/-- First positive rank clue on card `n` in `p`'s hand -/
+def positiveRank (p : C.C.Player) (n : ℕ) : Option C.C.Rank :=
+  S.positiveInfo p n |>.findSome? fun c ↦ c.getRight?
+
+/-- Negative color clues about card `n` in `p`'s hand (deduplicated) -/
+def negativeSuits (p : C.C.Player) (n : ℕ) : List C.C.Suit :=
+  (S.negativeInfo p n |>.filterMap fun c ↦ c.getLeft?).dedup
+
+/-- Negative rank clues about card `n` in `p`'s hand (deduplicated) -/
+def negativeRanks (p : C.C.Player) (n : ℕ) : List C.C.Rank :=
+  (S.negativeInfo p n |>.filterMap fun c ↦ c.getRight?).dedup
+
+/-- Possible color identities of card `n` in `p`'s hand
+(not taking into account other known cards in the discard pile or other hands) -/
+def possibleSuits (p : C.C.Player) (n : ℕ) : List C.C.Suit :=
+  match S.positiveSuit p n with
+    | some c => [c]
+    | none => (finRange C.numSuits).filter fun r ↦ r ∉ S.negativeSuits p n
+
+/-- Possible rank identities of card `n` in `p`'s hand
+(not taking into account other known cards in the discard pile or other hands) -/
+def possibleRanks (p : C.C.Player) (n : ℕ) : List C.C.Rank :=
+  match S.positiveRank p n with
+    | some r => [r]
+    | none => (finRange C.C.ranks).filter fun r ↦ r ∉ S.negativeRanks p n
+
+/-- Returns the empathy-known rank if it exists
+(not taking into account other known cards in the discard pile or other hands) -/
+def knownSuit (p : C.C.Player) (n : ℕ) : Option C.C.Suit :=
+  match S.possibleSuits p n with
+  | [c] => c
+  | _   => none
+
+/-- Returns the empathy-known rank if it exists
+(not taking into account other known cards in the discard pile or other hands) -/
+def knownRank (p : C.C.Player) (n : ℕ) : Option C.C.Rank :=
+  match S.possibleRanks p n with
+  | [r] => r
+  | _   => none
+
+/-- Returns the empathy-known card, if it is known
+(not taking into account other known cards in the discard pile or other hands) -/
+def knownCard (p : C.C.Player) (n : ℕ) : Option C.C.Card := do
+  let c ← S.knownSuit p n
+  let r ← S.knownRank p n
+  return (c, r)
+
+/-- The possible identities of the `n`-th card in player `p`'s hand -/
+def identities (p : C.C.Player) (n : ℕ) : List C.C.Card :=
+  S.possibleSuits p n |>.zip <| S.possibleRanks p n
+
+/-- Returns whether a card is currently empathy-playable
+(not taking into account other known cards in the discard pile or other hands) -/
+def empathyPlayable (p : C.C.Player) (n : ℕ) : Bool :=
+  S.identities p n |>.all S.playable
+
+/-- Returns whether a card is currently empathy-trash
+(not taking into account other known cards in the discard pile or other hands) -/
+def empathyTrash (p : C.C.Player) (n : ℕ) : Bool :=
+  S.identities p n |>.all S.trash
+
+/-- Returns whether a card is currently empathy-playable or trash
+(not taking into account other known cards in the discard pile or other hands) -/
+def isGoodTouchPlayable (p : C.C.Player) (n : ℕ) : Bool :=
+  S.identities p n |>.all S.playableOrTrash
+
+
+end PublicState
+end HanabiConfig
+
+/-- Information players conventionally keep track of. -/
+structure SimpleVariant.ConventionInfo (C : SimpleVariant) where
+  /-- The `n`-th card in player's `p` hand is conventionally playable -/
+  playable (p : C.C.Player) (n : ℕ) : Bool
+  /-- The `n`-th card in player's `p` hand is conventionally chop-moved -/
+  chopMoved (p : C.C.Player) (n : ℕ) : Bool
+  /-- The `n`-th card in player's `p` hand is conventionally known-trash -/
+  trash (p : C.C.Player) (n : ℕ) : Bool
+
+
+/-!
+Note: When we want to talk about simple variants, we have to write `C.C` for the `HanabiConfig`.
+-/
+variable {C : SimpleVariant} (I : C.ConventionInfo)
+
+namespace SimpleVariant.ConventionInfo
+
+/--
+Basic update of ConventionInfo:
+* accounts for the movement of cards
+  (perhaps we can change the encoding of cards so that we don't need to do this
+  i.e. there is a new type `Copy` (of a card) which is just the index in the initial deck that
+  is in the public state for every card)
+* marks empathy-playable cards as playable
+* marks empathy-trash cards as unplayable and trash
+-/
+def updateBasic (I : C.C.Info) (I' : ConventionInfo C) (_me p' : C.C.Player) :
+    C.C.Action → ConventionInfo C := fun
+  | .play n' => {
+    playable p n :=
+      ((if p != p' || n' < n then I'.playable p n else
+        if n == 0 then false else I'.playable p (n - 1))
+        && !I.empathyTrash p n) || I.empathyPlayable p n
+    chopMoved p n :=
+      if p != p' || n' < n then I'.chopMoved p n else
+        if n == 0 then false else I'.chopMoved p (n - 1)
+    trash p n :=
+      (if p != p' || n' < n then I'.trash p n else
+        if n == 0 then false else I'.trash p (n - 1))
+        || I.empathyTrash p n
+  }
+  | .discard n' => {
+    playable p n :=
+      if p != p' || n' < n then I'.playable p n else
+        if n == 0 then false else I'.playable p (n - 1)
+    chopMoved p n :=
+      if p != p' || n' < n then I'.chopMoved p n else
+        if n == 0 then false else I'.chopMoved p (n - 1)
+    trash p n :=
+      if p != p' || n' < n then I'.trash p n else
+        if n == 0 then false else I'.trash p (n - 1)
+  }
+  | .clue _ _ => {
+    playable p n := (I'.playable p n && !I.empathyTrash p n) || I.empathyPlayable p n
+    chopMoved p n := I'.chopMoved p n
+    trash p n := I'.trash p n || I.empathyTrash p n
+  }
+
+/-- A player's chop, taking into account touched and chop-moved cards. -/
+def conventionalChop (S : C.C.Info) (I : ConventionInfo C) (p : C.C.Player) : Option ℕ :=
+  (range C.C.handSize |>.filter fun n ↦ !S.touched p n && !I.chopMoved p n).getLast?
+
+/--
+The convention of giving color play clues with chop-focus.
+This convention applies if you touch a new (not touched or chop moved) card with a color clue.
+It is conventional if the card is currently playable.
+Extensions:
+- allow touching phantom-playable cards
+- make touching cards already touched in other player's hands unconventional
+- can be extended by allowing rank-plays, saves, finesses, bluffs, etc.
+-/
+def chopFocusColorPlay : C.toGame.Convention C.ConventionInfo where
+  IsConventional S I A :=
+    match A with
+    | .clue p (.inl s) =>
+      if h : p ≠ S.orig.me then
+        let newlyTouched : List ℕ :=
+          S.orig.willTouch p h (.inl s) |>.filter fun n ↦ !S.orig.touched p n && !I.chopMoved p n
+        if newlyTouched.length == 0 then
+          none
+        else
+          let target : ℕ :=
+            if newlyTouched.getLast? = conventionalChop S.orig I p then
+              newlyTouched.getLast! else newlyTouched.head!
+          some (S.orig.playable' p h target)
+      else
+        none
+    | _ => none
+  update S I :=
+    match S.prevAction with
+    | some (_, .clue p (.inl _)) =>
+      let newlyTouched : List ℕ :=
+        S.orig.lastTouched p |>.filter fun n ↦ !S.prev!.orig.touched p n && !I.chopMoved p n
+      if newlyTouched.length == 0 then
+        I
+      else
+        let target : ℕ :=
+          if newlyTouched.getLast? = conventionalChop S.prev!.orig I p then
+            newlyTouched.getLast! else newlyTouched.head!
+        { I with
+          playable p' n' := I.playable p' n' || (p' == p && n' == target) }
+    | _ => I
+
+
+
+
+-- /-- What newly clued card (positions) of the current hand of player `p`
+-- did the last clue to player `p` *newly* touch? -/
+-- def newlyTouched (p : C.Player) : List ℕ :=
+--   S.lastTouched p |>.findIdxs (·.head?.elim false (·.2))
+
+end SimpleVariant.ConventionInfo
 
 end Game
